2143e7575b796a411bfc0ba2026c0a5f958c803d
AGrabov <125709589+AGrabov@users.noreply.github.com>
2023-08-05 00:06:36 +0300

    Update live_trading.py

--- GMT_bot_02/live_trading.py
+++ GMT_bot_02/live_trading.py
@@ -36,7 +36,12 @@ notifier = TGnotify.TG_Notifier(tg_bot_api, tg_chat_id)
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Run live trading with the optimized "best_params".')
-optimized = parser.add_argument('--optimized', type=bool, default=False, help='Whether to use optimized parameters')
+parser.add_argument('--optimized', type=bool, default=False, help='Whether to use optimized parameters')
+
+args = parser.parse_args()
+
+# Use the arguments in your script
+optimized = args.optimized
 
 def write_current_params(params):
     with open('current_params.json', 'w') as f:
